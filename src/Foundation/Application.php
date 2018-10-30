9a41af753fe4def80e927db2fe4f001e5636a0a0
liangyu <liangyu@22.cn>
2018-10-30 12:00:04 +0800

    update

---
+++ src/Foundation/Application.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liangyu
+ * Date: 2018/10/30
+ * Time: 11:56
+ */
+
+namespace Wise\Foundation;
+
+use Pimple\Container;
+
+class Application extends Container
+{
+
+}
